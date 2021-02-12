ced3532822bb47cf392a6ee24ea195245e84c407
Marco Preciado <mac46005@gmail.com>
2021-02-11 21:15:16 -0600

    Added DB file to android/assets

---
+++ RiskGameTracker.Library/Models/PlayerModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RiskGameTracker.Library.Models
+{
+    public class PlayerModel
+    {
+        public int Id { get; set; }
+        public string PlayerName { get; set; }
+        public int ZonesOccupied { get; set; }
+        public int BonusAmount { get; set; }
+    }
+}
