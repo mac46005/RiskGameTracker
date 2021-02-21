1a62bdc16b92a18019b079ff603687bc59173725
Marco Preciado <mac46005@gmail.com>
2021-02-21 12:44:05 -0600

    Continue configuring PLayer Setup

--- RT_GUI/RT_GUI/ViewModels/PlayerCountViewModel.cs
+++ RT_GUI/RT_GUI/ViewModels/PlayerCountViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using RT_GUI.Views;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Text;
 using System.Windows.Input;
 using Xamarin.Forms;
@@ -11,12 +12,22 @@ namespace RT_GUI.ViewModels
     {
         public PlayerCountViewModel()
         {
-
+            OnPageLoad();
         }
+        public int playerCount = 0;
         public int SliderValue { get; set; }
-        ICommand OnButtonClicked => new Command(async () =>
+        public ObservableCollection<PLayerViewModel> MyProperty { get; set; }
+        public ICommand OnButtonClicked => new Command(async () =>
         {
             await Navigation.PushAsync(Resolver.Resolve<PlayerSetupView>());
         });
+        private void OnPageLoad()
+        {
+            CreatePlayer();
+        }
+        private void CreatePlayer()
+        {
+
+        }
     }
 }
